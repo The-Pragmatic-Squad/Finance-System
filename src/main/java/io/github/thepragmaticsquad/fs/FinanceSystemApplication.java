79d0c4ad4397f71ac44e4cb14f3d17be4eec1767
Muhammad <21185758+Muhammad-Saad-01@users.noreply.github.com>
2023-06-14 23:22:10 +0300

    Getting Started

---
+++ src/main/java/io/github/thepragmaticsquad/fs/FinanceSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.thepragmaticsquad.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSystemApplication.class, args);
+	}
+
+}
