094fdcedf35626956855dccd04c1119fcb4afda1
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2023-06-24 21:02:45 +0300

    Refactoring

--- src/main/java/io/github/thepragmaticsquad/fs/Repository/AccountRepository.java
+++ src/main/java/io/github/thepragmaticsquad/fs/Repository/AccountRepository.java
@@ -3,7 +3,6 @@ package io.github.thepragmaticsquad.fs.repository;
 import io.github.thepragmaticsquad.fs.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
